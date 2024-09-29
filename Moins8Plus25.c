a1b51de636ea616b8ce5de5fb5f25b3fb5586bb6
Sebman56 <113142393+Sebman56@users.noreply.github.com>
2024-09-29 02:21:36 +0200

    Add files via upload
    Correction du fichier Moins8Plus25.c

--- Moins8Plus25.c
+++ Moins8Plus25.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 #include "prototype.h"
 
+int Somme;
+
 int CalculMoins8Plus25 ()
 {
+    Somme = A+ B + C + D + E;
     printf ("\n\t Calcul du moins 8 et plus 25.");
-    if ((A + B + C + D + E) < 8)
+    if ((Somme) < 8)
     {printf ("\n\t\t Moins 8 détecté.\n");}
 
-    if ((A + B + C + D + E) > 25)
+    if ((Somme) > 25)
     {printf ("\n\t\t Plus 25 détecté.\n");}
 
-    if (((A + B + C + D + E) >= 8) || ((A + B + C + D + E) <= 25)) 
-    {printf ("\n\t\t Pas de moins 8 ou de plus 25 détecté.\n");}
+    if ((Somme >= 8) && (Somme <= 25)) 
+    {printf ("\n\t\t Pas de moins 8 ou de plus 25 détecté. --- %i --- \n",Somme);}
 }
